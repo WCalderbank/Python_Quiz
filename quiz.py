944f02f4d3844063f53589e88b56557e4b6b2d0f
WCalderbank <wcalderbank99@gmail.com>
2022-06-20 14:52:47 +0100

    point system added

--- quiz.py
+++ quiz.py
@@ -6,10 +6,12 @@ if play.lower() != "yes" :
     quit()
 
 print("Excellent, Lets Begin...")
+points = 0
 
 question = input("In Star Wars, Which Character Debuted With A Red Double-bladed Lightsaber? ")
 if question.lower() == "darth maul":
     print("Correct! :)")
+    points = points + 1
 else: 
     print("Sorry that's Incorrect! :( ")
 
@@ -17,6 +19,7 @@ else:
 question = input("In 'The Witcher', Geralt has a steel sword and a ____ sword? ")
 if question.lower() == "silver":
     print("Correct! :)")
+    points = points + 1
 else: 
     print("Sorry that's Incorrect! :( ")
 
@@ -24,6 +27,7 @@ else:
 question = input("What symbol is House Tyrell in 'Game of Thrones'? ")
 if question.lower() == "rose":
     print("Correct! :)")
+    points = points + 1
 else: 
     print("Sorry that's Incorrect! :( ")
 
@@ -31,6 +35,7 @@ else:
 question = input("What year does Marty McFly get sent too in 'Back to the Future'(1985)?  ")
 if question.lower() == "1955":
     print("Correct! :)")
+    points = points + 1
 else: 
     print("Sorry that's Incorrect! :( ")
 
@@ -38,6 +43,9 @@ else:
 question = input("In the series 'My Hero Academia', which character has the quirk of fire and ice?  ")
 if question.lower() == "todoroki":
     print("Correct! :)")
+    points = points + 1
 else: 
     print("Sorry that's Incorrect! :( ")
 
+print("Congratulations, You Got " + str(points) + " Questions Correct!")
+print("You Got " + str((points/5) * 100) + "%")
