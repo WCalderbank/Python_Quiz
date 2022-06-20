7b54d078e7fb8d6cb295dcb73ddada2a01535013
WCalderbank <wcalderbank99@gmail.com>
2022-06-20 13:49:03 +0100

    Initial Commit

---
+++ quiz.py
@@ -0,0 +1,43 @@
+print("Hello There, Welcome!")
+
+play = input("Would you like to play a game? (Yes or No)")
+
+if play != "Yes" :
+    quit()
+
+print("Excellent, Lets Begin...")
+
+question = input("In Star Wars, Which Character Debuted With A Red Double-bladed Lightsaber? ")
+if question == "Darth Maul":
+    print("Correct! :)")
+else: 
+    print("Sorry that's Incorrect! :( ")
+
+
+question = input("In 'The Witcher', Geralt has a steel swords and a ____ sword?")
+if question == "Silver":
+    print("Correct! :)")
+else: 
+    print("Sorry that's Incorrect! :( ")
+
+
+question = input("What symbol is House Tyrell in 'Game of Thrones'?")
+if question == "Rose":
+    print("Correct! :)")
+else: 
+    print("Sorry that's Incorrect! :( ")
+
+
+question = input("What year does Marty get sent too in 'Back to the Future'(1985)? ")
+if question == "1955":
+    print("Correct! :)")
+else: 
+    print("Sorry that's Incorrect! :( ")
+
+
+question = input("In the series 'My Hero Academia', which character has the quirk of fire and ice? ")
+if question == "Todoroki":
+    print("Correct! :)")
+else: 
+    print("Sorry that's Incorrect! :( ")
+
