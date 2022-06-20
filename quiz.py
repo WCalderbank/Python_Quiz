fd220a988578b0cfbe440e793fe3453d574029da
WCalderbank <wcalderbank99@gmail.com>
2022-06-20 14:22:38 +0100

    set answers to lowercase

--- quiz.py
+++ quiz.py
@@ -1,42 +1,42 @@
 print("Hello There, Welcome!")
 
-play = input("Would you like to play a game? (Yes or No)")
+play = input("Would you like to play a game? (yes or no) ")
 
-if play != "Yes" :
+if play.lower() != "yes" :
     quit()
 
 print("Excellent, Lets Begin...")
 
 question = input("In Star Wars, Which Character Debuted With A Red Double-bladed Lightsaber? ")
-if question == "Darth Maul":
+if question.lower() == "darth maul":
     print("Correct! :)")
 else: 
     print("Sorry that's Incorrect! :( ")
 
 
-question = input("In 'The Witcher', Geralt has a steel swords and a ____ sword?")
-if question == "Silver":
+question = input("In 'The Witcher', Geralt has a steel sword and a ____ sword? ")
+if question.lower() == "silver":
     print("Correct! :)")
 else: 
     print("Sorry that's Incorrect! :( ")
 
 
-question = input("What symbol is House Tyrell in 'Game of Thrones'?")
-if question == "Rose":
+question = input("What symbol is House Tyrell in 'Game of Thrones'? ")
+if question.lower() == "rose":
     print("Correct! :)")
 else: 
     print("Sorry that's Incorrect! :( ")
 
 
-question = input("What year does Marty get sent too in 'Back to the Future'(1985)? ")
-if question == "1955":
+question = input("What year does Marty McFly get sent too in 'Back to the Future'(1985)?  ")
+if question.lower() == "1955":
     print("Correct! :)")
 else: 
     print("Sorry that's Incorrect! :( ")
 
 
-question = input("In the series 'My Hero Academia', which character has the quirk of fire and ice? ")
-if question == "Todoroki":
+question = input("In the series 'My Hero Academia', which character has the quirk of fire and ice?  ")
+if question.lower() == "todoroki":
     print("Correct! :)")
 else: 
     print("Sorry that's Incorrect! :( ")
